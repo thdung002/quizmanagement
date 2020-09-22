6157b6c723b7421a6730d5839049912ec3adb32d
Dũng <thdung002@gmail.com>
2020-09-22 14:24:06 +0700

    Dung merge file

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,69 @@
+'use strict';
+const User = require('../models/user.model');
+
+module.exports = {
+    //create a new user
+    addUser: function (req, res) {
+        let role = req.body.accessUserRole || '';
+        let id = req.body.accessID || ' ';
+        let new_user = req.body || '';
+        //handles null error
+        if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+            res.status(400).send({error: true, message: 'Please provide all required field'});
+        } else {
+            User.addUser(id,role,new_user, function (err, result) {
+                if (err)
+                    res.send(err);
+                res.json({error: false, message: "User added successfully!", data: result});
+            });
+        }
+    },
+    //get all user
+    getUser: function (req, res) {
+        User.getUser(function (err, result) {
+            if (err)
+                res.send(err);
+            console.log('result', result);
+            res.send(result);
+        })
+    },
+    //get 1 user by id
+    getUserById: function (req, res) {
+        User.getUserById(req.params.id, function (err, result) {
+            if (err)
+                res.send(err);
+            res.json(result);
+        })
+    },
+    //update user
+    updateUserById: function (req, res) {
+        if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+            res.status(400).send({error: true, message: 'Please provide all required field'});
+        } else {
+            User.updateUserById(req.params.id, new User(req.body), function (err, result) {
+                if (err)
+                    res.send(err);
+                res.json({error: false, result});
+            });
+        }
+    },
+    //delete user
+    deleteUserById: function (req, res) {
+        User.deleteUserById(req.params.id, function (err, result) {
+            if (err)
+                res.send(err);
+            res.json({error: false, result});
+        })
+    },
+    //check for login authenticate
+    login:function(req,res){
+        let loginName = req.body.loginName || '';
+        let password = req.body.password || '';
+        User.authenticate(loginName,password,function(err,result){
+            if(err)
+                res.send(err);
+            res.json({error:false,result});
+        })
+    }
+
+};
---
+++ Backend/src/routes/user.route.js
@@ -0,0 +1,9 @@
+const userController =   require('../controllers/user.controller');
+module.exports = function (app) {
+    app.get('/v1/user/get', userController.getUser);
+    app.post('/v1/user/add', userController.addUser);
+    app.get('/v1/user/get/:id', userController.getUserById);
+    app.put('/v1/user/update/:id', userController.updateUserById);
+    app.delete('/v1/user/delete/:id', userController.deleteUserById);
+    app.post('/v1/user/login',userController.login);
+};
