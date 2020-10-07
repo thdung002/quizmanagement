5e6ebb22b422e48a380e35d8b4b731af36b7d2e9
TrungPVQ <leot.pvq@gmail.com>
2020-10-07 11:01:47 +0700

    Trung - Topic - 1

--- Backend/src/models/topic.js
+++ Backend/src/models/topic.js
@@ -8,53 +8,52 @@ var Topic = function (topic) {
     this.UpdatedBy = topic.UpdatedBy;
 
 };
-
-//Add new Topic
-Topic.add = function (accessID,newTop, result) {
+//Add new topic
+Topic.add = function (accessID, newTopic, result) {
     if (!Pieces.VariableBaseTypeChecking(newTopic.Content, 'string')
-        || newAnswer.Content===null) {
-        return result(1, 'invalid_Answer', 400, null, null);
-    }else {
+        || newTopic.Content === null) {
+        return result(1, 'Content null', 400, null, null);
+    }
+    else {
         try {
             let queryObj = {};
             queryObj.Content = newTopic.Content;
             queryObj.CreatedBy = accessID;
-            dbConn.query("INSERT INTO Topic set ?", queryObj, function (err, res) {
+            dbConn.query("INSERT INTO topic set ?", queryObj, function (err, res) {
                 if (err) {
                     result(err, null);
                 } else {
-                    console.log(res.insertId);
-                    result(null, res.insertId);
+                    result(null, res.err);
                 }
             });
         } catch (error) {
-            result(1, 'create_Answer_fail', 400, error, null);
+            result(1, 'create_Topic_fail', 400, error, null);
         }
     }
 };
-//Get 1 answer by ID
+//Get 1 topic by ID
 Topic.getTopicById = function (id, result) {
     try {
-        dbConn.query("Select * from Answer where question = ? ", parseInt(id), function (err, res) {
-                if (err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                } else if(res.length === 0)
-                    result (1, 'Answer_not_found', 403, err, null);
-                else {
-                    result(null, res);
-                }
+        dbConn.query("Select * from topic where id = ? ", parseInt(id), function (err, res) {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+            } else if (res.length === 0)
+                result(1, 'No topic found', 403, err, null);
+            else {
+                result(null, res);
             }
+        }
         );
     } catch (error) {
-        return result(1, 'get_one_Answer_fail', 400, error, null);
+        return result(1, 'Get topic fail', 400, error, null);
     }
 };
-//Get all answer
-Topic.getTopic = function (result) {
-    if (page === 0)
+//Get all topic
+Topic.getTopic = function (page, perpage, sort, result) {
+    if (page === 0 || isNaN(page))
         page = 1;
-    if (perpage <= 0) {
+    if (perpage <= 0 || isNaN(perpage)) {
         perpage = 5;
     }
     if (sort.length === 0) {
@@ -63,11 +62,11 @@ Topic.getTopic = function (result) {
     let type = typeof (sort);
     let offset = perpage * (page - 1);
     try {
-        dbConn.query("SELECT COUNT(*) as total from Topic ", function (err, rows) {
+        dbConn.query("SELECT COUNT(*) as total from topic ", function (err, rows) {
             if (err) {
                 return result(err);
             } else {
-                dbConn.query(`Select * from Topic ORDER BY ID ${sort} limit ${perpage} offset ${offset} `, function (errs, res) {
+                dbConn.query(`Select * from topic ORDER BY ID ${sort} limit ${perpage} offset ${offset} `, function (errs, res) {
                     if (errs) {
                         console.log("error in query db: ", errs);
                         return result(errs);
@@ -100,24 +99,41 @@ Topic.getTopic = function (result) {
         return result(1, 'get_all_Topic_fail', 400, error, null);
     }
 };
-Topic.updateTopicById = function (id, topic, result) {
-    dbConn.query("UPDATE topic SET Content=?,CreatedBy=?,UpdatedBy=? WHERE id = ?", [topic.Content, topic.CreatedBy, topic.UpdatedBy, id], function (err, res) {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-        } else {
-            result(null, res);
-        }
-    });
+//Update Topic by id
+Topic.update = function (id, accessID, Topic, result) {
+    try {
+        let queryObj = {};
+        queryObj.Content = Topic.Content;
+        queryObj.UpdatedBy = accessID;
+        queryObj.Id = id;
+        dbConn.query("UPDATE topic SET Content=?,UpdatedBy=? WHERE id = ?", [queryObj.Content, queryObj.UpdatedBy, queryObj.Id], function (err, res) {
+            if (err) {
+                result(null, err);
+            } else if (res.changedRows === 0)
+                result(1, 'Nothing was updated', 403, err, null);
+            else {
+                result(null, queryObj.Id);
+            }
+        });
+    } catch (error) {
+        return result(1, 'update_Topic_fail', 400, error, null);
+    }
 };
-Topic.deleteTopicById = function (id, result) {
-    dbConn.query("DELETE FROM topic WHERE id = ?", [id], function (err, res) {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-        } else {
-            result(null, res);
-        }
-    });
+//Delete 1 topic by id
+Topic.delete = function (id, result) {
+    try {
+        dbConn.query("DELETE FROM topic WHERE id = ?", [id], function (err, res) {
+            if (err) {
+                console.log("error: ", err);
+                result(null, err);
+            } else if (res.affectedRows === 0)
+                result(1, 'Topic_not_found', 403, err, null);
+            else {
+                result(null, id);
+            }
+        });
+    } catch (error) {
+        return result(1, 'delete_Topic_fail', 400, error, null);
+    }
 };
 module.exports = Topic;
