167ba59c3d4531f983cb5e7163cae0127e1003db
Dũng <thdung002@gmail.com>
2020-12-16 13:08:22 +0700

    deploy website again

--- frontend/gh-pages-deploy.js
+++ frontend/gh-pages-deploy.js
@@ -3,19 +3,22 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "deploy-page-test"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
-    await execa("npm", ["run", "build"]);
+    await execa("npm", ["run", "build:prod"]);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const folderName = fs.existsSync("dist") ? "dist" : "build:prod";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rm", ["-r", folderName]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "deploy-page-test"]);
+    console.log("Pushing to deploy-page-test...");
+    await execa("git", ["push", "origin", "HEAD:deploy-page-test", "--force"]);
+    console.log("Delete folder name"+folderName);
+    await execa("del", ["-r", folderName]);
+    console.log("Checkout master...");
     await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Branch...");
+    await execa("git", ["branch", "-D", "deploy-page-test"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
