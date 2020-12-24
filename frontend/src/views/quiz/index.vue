ce1f16e826004c2fdf54c3ac17d5a20d2de3722a
Dũng <thdung002@gmail.com>
2020-12-24 19:09:55 +0700

    edit

--- frontend/src/views/quiz/index.vue
+++ frontend/src/views/quiz/index.vue
@@ -13,10 +13,6 @@
       <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         Search
       </el-button>
-      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit"
-                 @click="handleCreate">
-        Add
-      </el-button>
       <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download"
                  @click="handleDownload">
         Export
@@ -71,13 +67,6 @@
           </el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
-        <template slot-scope="{row,$index}">
-          <el-button type="primary" size="mini" @click="handleUpdate(row)">
-            View
-          </el-button>
-        </template>
-      </el-table-column>
     </el-table>
     <!--Table for values-->
 
@@ -86,47 +75,6 @@
 
     <!--pagination-->
 
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form ref="dataForm" :model="temp" label-position="left" label-width="150px"
-               style="width: 400px; margin-left:50px;">
-        <el-form-item label="Examination id" prop="Examination">
-          <el-input :disabled="true" v-model="temp.Examination"/>
-        </el-form-item>
-        <el-form-item label="Template id" prop="Template">
-          <el-input :disabled="true" v-model="temp.Template"/>
-        </el-form-item>
-        <el-form-item label="Config id" prop="Config">
-          <el-input :disabled="true" v-model="temp.Config"/>
-        </el-form-item>
-        <el-form-item label="Code" prop="Code">
-          <el-input :disabled="true" v-model="temp.Code"/>
-        </el-form-item>
-        <el-form-item label="Status" prop="IsDeleted">
-          <el-select :disabled="true" v-model="temp.IsDeleted" class="filter-item" placeholder="Please select">
-            <el-option v-for="item in statusType" :key="item.key" :label="item.display_name" :value="item.key"/>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">
-          Cancel
-        </el-button>
-        <!-- <el-button type="primary" @click="dialogStatus==='create'? createData():updateData()">
-          Print
-        </el-button> -->
-      </div>
-    </el-dialog>
-    <!--    Hidden dialog-->
-
-    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
-      <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
-        <el-table-column prop="key" label="Channel"/>
-        <el-table-column prop="pv" label="Pv"/>
-      </el-table>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
-      </span>
-    </el-dialog>
   </div>
 </template>
 
@@ -250,15 +198,6 @@
                     accessUserRole: '',
                     IsDeleted: 0,
                 }
-            },
-            handleCreate() {
-                this.resetTemp();
-                this.dialogStatus = 'create';
-                this.dialogFormVisible = true;
-                this.listQuery.content = undefined;
-                this.$nextTick(() => {
-                    this.$refs.dataForm.clearValidate()
-                })
             },
             handleUpdate(row) {
                 this.temp = Object.assign({}, row); // copy obj
